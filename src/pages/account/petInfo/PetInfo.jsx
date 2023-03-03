a0ccc6a126be99e22067a3a8adbfba525bdd33b8
Ignacio Rossetto <ignaciorossetto@hotmail.com>
2023-03-02 22:56:35 -0300

    v1.1

--- src/pages/account/petInfo/PetInfo.jsx
+++ src/pages/account/petInfo/PetInfo.jsx
@@ -1,18 +1,13 @@
-import { faCalendarDays } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import Swal from "sweetalert2";
+import {  useNavigate } from "react-router-dom";
 import EmailSubs from "../../../components/emailSubs/EmailSubs";
 import Footer from "../../../components/footer/Footer";
 import Navbar from "../../../components/navbar/Navbar";
 import Skeleton from "../../../components/skeleton/Skeleton";
 import { AuthContext } from "../../../context/AuthContext";
 import "./petInfo.css";
-import { DateRange } from "react-date-range";
-import { format } from "date-fns";
-import Switch from "../../../components/switch/Switch";
 import PetManagerCard from "../../../components/petManagerCard/PetManagerCard";
 import config from "../../../config/config.js";
 
@@ -23,40 +18,8 @@ const PetInfo = () => {
   const [fetching, setFetching] = useState(true);
   const [updater, setUpdater] = useState(1);
   const navigate = useNavigate();
-  const [openCalendar, setOpenCalendar] = useState(false);
-  const [date, setDate] = useState([
-    {
-      startDate: new Date(),
-      endDate: new Date(),
-      key: "selection",
-    },
-  ]);
 
-  const handleDeletePetClick = async (id, pid) => {
-    Swal.fire({
-      title: "Estas seguro?",
-      text: "No se puede revertir este cambio!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#d33",
-      cancelButtonColor: "#44ad36",
-      confirmButtonText: "Si, borrar mascota!",
-      cancelButtonText: "No borrar mascota!",
-    }).then(async (result) => {
-      if (result.isConfirmed) {
-        Swal.fire("Deleted!", "Your file has been deleted.", "success");
-        try {
-          await axios.delete(`${config.url}/api/pets/${id}/${pid}`);
-          await axios.get(`${config.url}/api/auth/updateUser`);
-          setUpdater((prev) => prev + 1);
-        } catch (error) {
-          navigate("/error", {
-            state: { error: "Ocurrio un error, intenta mas tarde" },
-          });
-        }
-      }
-    });
-  };
+
 
   const fetchAuth = async () => {
     setFetching(true);
